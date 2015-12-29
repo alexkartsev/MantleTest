4cb88eb923eba155781729dd6eaae7d64306cfc9
Alexander <sanekkartsev@gmail.com>
2015-12-29 20:28:27 +0300

    add view controller

--- MantleTest/AppDelegate.m
+++ MantleTest/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    MainViewController *vc = [[MainViewController alloc] init];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:vc];
+    self.window.rootViewController = navController;
     return YES;
 }
 
