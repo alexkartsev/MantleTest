05484b5c3bc28f6fa496e8303870b6da1f2bc4d3
Alexander <sanekkartsev@gmail.com>
2015-12-29 20:20:02 +0300

    init commit

---
+++ MantleTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MantleTest
+//
+//  Created by Alexander on 12/29/15.
+//  Copyright © 2015 iTechArt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MantleTest/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MantleTest
+//
+//  Created by Alexander on 12/29/15.
+//  Copyright © 2015 iTechArt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
